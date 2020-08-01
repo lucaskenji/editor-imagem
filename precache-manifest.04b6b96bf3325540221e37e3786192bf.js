e5da476692f643b9c2e02add47d900489028cc7b
Lucas <ra1957066@gmail.com>
2020-08-01 00:22:27 -0300

    Updates

---
+++ precache-manifest.04b6b96bf3325540221e37e3786192bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4601fc44eed8e468c4876e4c16acb65b",
+    "url": "/editor-imagem/index.html"
+  },
+  {
+    "revision": "0f02ff84e70ba2a8232a",
+    "url": "/editor-imagem/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "65fb8611e401c7a6c1b4",
+    "url": "/editor-imagem/static/css/main.d0e78f77.chunk.css"
+  },
+  {
+    "revision": "0f02ff84e70ba2a8232a",
+    "url": "/editor-imagem/static/js/2.43a096e6.chunk.js"
+  },
+  {
+    "revision": "031a228f78b6c9e6d628fc885d7d626a",
+    "url": "/editor-imagem/static/js/2.43a096e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fb8611e401c7a6c1b4",
+    "url": "/editor-imagem/static/js/main.3c8725f9.chunk.js"
+  },
+  {
+    "revision": "4c85c90980bc82167707",
+    "url": "/editor-imagem/static/js/runtime-main.667e61b8.js"
+  },
+  {
+    "revision": "271a1b9467994dfdb1207a63770faac6",
+    "url": "/editor-imagem/static/media/JockeyOne-Regular.271a1b94.ttf"
+  },
+  {
+    "revision": "687fd5f255252a2061c08d5c0d03b65d",
+    "url": "/editor-imagem/static/media/default_image.687fd5f2.png"
+  }
+]);
